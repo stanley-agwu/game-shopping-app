9c7132db8fb0bee16ef7ac1847b1b6899c4da52a
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-09-23 19:49:23 +0200

    feat(app): Card item

---
+++ game-shopping-app/src/common/components/CardItem.tsx
@@ -0,0 +1,33 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+export default function MultiActionAreaCard() {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component='img'
+          height='140'
+          image='/static/images/cards/contemplative-reptile.jpg'
+          alt='green iguana'
+        />
+        <CardContent>
+          <Typography gutterBottom variant='h5' component='div'>
+            Name
+          </Typography>
+          <Typography variant='body2' color='text.secondary'>
+            Description
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size='small' color='primary'>
+          Add to cart
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
