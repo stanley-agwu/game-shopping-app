db5d05344799d1e4c875c90f1b7bfada33318c35
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-09-23 21:08:17 +0200

    feat(app): Navigation Menu button - updated

--- game-shopping-app/src/common/components/NavigationMenu.tsx
+++ game-shopping-app/src/common/components/NavigationMenu.tsx
@@ -24,9 +24,9 @@ const NavigationMenu = () => {
         </NavigationMenuItem>
       </NavigationMenuList>
       <button className='list-none flex items-center relative'>
-        <ShoppingCartIcon className='bg-slate-100 rounded-full p-2 cursor-pointer' />
+        <ShoppingCartIcon className='bg-slate-100 rounded-full p-2 cursor-pointer border border-indigo-600 hover:bg-indigo-300 hover:fill-white hover:border-2 focus:bg-indigo-300 focus:fill-white focus:border-2' />
         <div
-          className='rounded-full bg-red-700 flex justify-center items-center p-1 w-7 h-7 absolute text-white text-xs font-medium'
+          className='rounded-full bg-red-700 border-2 border-red-700 flex justify-center items-center p-1 w-7 h-7 absolute text-white text-xs font-medium'
           style={{ transform: 'translate(95%, 95%)' }}
         >
           125
