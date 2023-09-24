eb0d1f9bac6cd0cc0d4904963ac67228e420f6e0
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-09-24 11:09:23 +0200

    feat(app): Loader component

---
+++ game-shopping-app/src/common/components/Loader.tsx
@@ -0,0 +1,45 @@
+import Lottie from 'lottie-react';
+
+import animationData from './ui/animationData.json';
+
+export interface LoaderProps {
+  width?: number;
+  className?: string;
+  height?: number;
+}
+
+const Loader = ({
+  width = 100,
+  height = 100,
+  className,
+  ...props
+}: LoaderProps): JSX.Element => {
+  const defaultOptions = {
+    ...props,
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice',
+    },
+  };
+  return (
+    <div
+      role='botton'
+      tabIndex={0}
+      aria-label='animation'
+      className={className}
+      style={{
+        width: `${width}px`,
+        height: `${height}px`,
+        overflow: 'hidden',
+        margin: 'auto',
+        outline: 'none',
+      }}
+    >
+      <Lottie {...defaultOptions} />
+    </div>
+  );
+};
+
+export default Loader;
