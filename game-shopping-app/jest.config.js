3bafb344cf1a9b0d9470731e719d79392d4db537
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2023-10-02 23:59:29 +0200

    feat(app): set up babel, jest config

---
+++ game-shopping-app/jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  preset: 'ts-jest',
+  bail: 1,
+  verbose: true,
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTest.ts'],
+  moduleDirectories: ['node_modules', 'test', 'src'],
+  collectCoverage: true,
+  coverageReporters: ['text'],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+  ],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    'index.tsx',
+    '<rootDir>/src/main.tsx',
+  ],
+  coverageDirectory: '<rootDir>/coverage/',
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
+    },
+  },
+};
