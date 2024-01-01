3963a94bf05965e83a8812a6e4604a5eab047f2a
stanleyagwu2016@gmail.com <stanleyagwu2016@gmail.com>
2024-01-01 15:19:25 +0100

    feat(app): use unique keys/ids for ratings

--- game-shopping-app/src/modules/shop/components/Rating.tsx
+++ game-shopping-app/src/modules/shop/components/Rating.tsx
@@ -1,8 +1,8 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import RateIcon from '@/assets/star-empty.svg';
 import RateFillIcon from '@/assets/star-filled.svg';
 
 import { getRating } from '../utils/shop-utils';
+import { v4 as uuidv4 } from 'uuid';
 
 interface RatingProps {
   rate: number;
@@ -13,11 +13,11 @@ function Rating({ rate, reviews }: RatingProps) {
   const rating = getRating(rate);
   return (
     <div className="flex py-2 items-center">
-      {[...Array(5)].map((idx: number) =>
-        rating > idx ? (
-          <img src={RateFillIcon as string} key={idx} />
+      {[...Array(5)].map((_, idx) =>
+        rating >= idx ? (
+          <img src={RateFillIcon as string} key={uuidv4()} alt="Rating filled star" />
         ) : (
-          <img src={RateIcon as string} key={idx} />
+          <img src={RateIcon as string} key={uuidv4()} alt="Rating empty star" />
         )
       )}
       <span className="ml-4 font-medium text-sm">{reviews} reviews</span>
